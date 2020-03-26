a9e1d7d49ee13d30eac7ddb9d35b2fd1ae35af28
Kray-G <nagai.kazuya@gmail.com>
2020-03-26 21:05:31 +0900

    added fontawesome.

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 library.add(fas, far, fab)
-
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
